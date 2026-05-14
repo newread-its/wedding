ebc57ed46980466b1122bbc66ca05bea96c07ec0
newread-its <newread.its@gmail.com>
2026-05-14 21:39:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,4 +87,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+const reveals = document.querySelectorAll(".reveal");
 
+function revealOnScroll() {
+
+    reveals.forEach((element) => {
+
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+
+        if(elementTop < windowHeight - 100){
+            element.classList.add("active");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
