484a533e47f2c64ebd3ceae9188855d2190a90b9
newread-its <newread.its@gmail.com>
2026-06-09 21:57:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -177,7 +177,9 @@ async function loadWishes(){
 
                 </div>
 
-                <div class="wish-message">${item.pesan || "-"}</div>
+                <div class="wish-message" dir="auto">
+                    ${item.pesan || "-"}
+                </div
 
             `;
 
