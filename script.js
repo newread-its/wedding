3ffa7f225df3f5a66eae5d66d32660cef1649251
newread-its <newread.its@gmail.com>
2026-06-09 23:39:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ if (nama) {
 // COUNTDOWN
 // =========================
 const target =
-    new Date("Jun 12, 2026 07:30:00").getTime();
+new Date("2026-06-12T07:30:00+07:00").getTime();
 
 setInterval(() => {
 
